9c1146ff7f540744a40d62d4fc5e4b0dbbd3e641
melviinn <105702311+melviinn@users.noreply.github.com>
2023-02-09 10:04:12 +0100

    Update FaillesController.php

--- src/Controller/FaillesController.php
+++ src/Controller/FaillesController.php
@@ -3,7 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\FAILLES;
+use App\Entity\FAILLESAPPLICATIONS;
+use App\Form\FaillesAppsType;
 use App\Form\FaillesType;
+use App\Repository\FAILLESAPPLICATIONSRepository;
 use App\Repository\FAILLESRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
@@ -24,16 +27,19 @@ class FaillesController extends AbstractController
         return $this->render('failles/liste.html.twig');
     }
 
-    public function detailsFailles(Request $request, FAILLESRepository $faillesRepo, $id)
+    public function detailsFailles(Request $request, FAILLESRepository $faillesRepo, $faille_id)
     {
         // Récupération de l'id de la faille choisie dans le tableau JqxGrid (gridDetailsFailles.js)
-        $id = $request->get('id');
+        $faille_id = $request->get('faille_id');
+        //dd($id);
+        $test = $faillesRepo->findDetailsFaillesSQLById($faille_id);
+        //dd($test);
         // Récupération du libellé de la faille (On récupère l'entité puis le libellé grâce à la fonction __toString())
-        $faille_lib = $faillesRepo->findOneBy(['id' => $id]);
+        $faille_lib = $faillesRepo->findOneBy(['id' => $faille_id]);
 
         return $this->render('failles/details.html.twig', [
             'faille_lib' => $faille_lib,
-            'faille_id' => $id
+            'faille_id' => $faille_id
         ]);
     }
 
@@ -43,13 +49,38 @@ class FaillesController extends AbstractController
         $failles = new FAILLES();
 
         // Création du formulaire
-        $form = $this->createForm(FaillesType::class, $failles);
+        $form = $this->createForm(FaillesAppsType::class);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
 
+            $LIB_FAILLE = $form->get('LIB_FAILLE')->getData();
+            $COMP_FAILLE = $form->get('COMP_FAILLE')->getData();
+            $DATE_FAILLE = $form->get('DATE_FAILLE')->getData();
+            $STATUT = $form->get('STATUT')->getData();
+            $DATE_FERMETURE = $form->get('DATE_FERMETURE')->getData();
+            $ID_APPLICATION = $form->get('ID_APPLICATION')->getData();
+            
+
             // Persist des données
+            $failles->setLIBFAILLE($LIB_FAILLE);
+            $failles->setCOMPFAILLE($COMP_FAILLE);
+            $failles->setDATEFAILLE($DATE_FAILLE);
+            $failles->setSTATUT($STATUT);
+            $failles->setDATEFERMETURE($DATE_FERMETURE);
+
             $em->persist($failles);
+            $em->flush();
+
+            foreach($ID_APPLICATION as $a) {
+                $faillesApplis = new FAILLESAPPLICATIONS();
+                $faillesApplis->setIDAPPLICATION($a);
+                $faillesApplis->setIDFAILLE($failles);
+                $faillesApplis->setTRAITEMENT('En cours d\'analyse');
+
+                $em->persist($faillesApplis);
+            }
+
             $em->flush();
 
             // Message de confirmation...
@@ -66,11 +97,37 @@ class FaillesController extends AbstractController
         ]);
     }
 
+    public function modifierFailles(Request $request, EntityManagerInterface $em, FAILLESRepository $faillesRepo, FAILLESAPPLICATIONSRepository $faillesAppsRepo, int $id)
+    {
+        // Récupération de l'id ainsi que les données de la table FAILLES en fonction de l'id reçu
+        $id = $request->get('id');
+        $FAILLES = $faillesRepo->findFaillesById($id);
+        //$test = $faillesAppsRepo->findFaillesSQLById($id);
+        //$test2 = $faillesAppsRepo->findTest($id);
+        //dd($test2);
+
+        // Création du formulaire
+        $form = $this->createForm(FaillesAppsType::class, $FAILLES);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            // Persist des données
+            $em->persist($FAILLES);
+            $em->flush();
+
+            return $this -> redirectToRoute('app_ref_failles');
+        }
+
+        return $this->render('failles/modifier.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
     public function listFaillesToJson(FAILLESRepository $faillesRepo)
     {
         // Récupération des données au format array()
         $listFailles = $faillesRepo->findAllFaillesSQLToArray();
-
         // Envoi des données au format JSON -> JqxGrid (gridFailles.js)
         return new JsonResponse($listFailles);
     }
@@ -82,7 +139,7 @@ class FaillesController extends AbstractController
         // Récupération des détails de chaque faille en fonction de son id aux format array()
         $listFailles = $faillesRepo->findDetailsFaillesSQLById($faille_id);
 
-        // Envoir des données au format JSON ->JqxGrid (gridDetailsFailles.js)
+        // Envoie des données au format JSON ->JqxGrid (gridDetailsFailles.js)
         return new JsonResponse($listFailles);
     }
 
@@ -136,6 +193,7 @@ class FaillesController extends AbstractController
             $sheet->getColumnDimension($colString)->setAutoSize(true);
         }
 
+        // Alignement horizontal des valeurs
         $sheet->getStyle('D2:E'.(sizeof($listFailles)+1))->getAlignment()->setHorizontal(Alignment::HORIZONTAL_LEFT);
 
         // Insertion des données
